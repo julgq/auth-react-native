ef5dbacedf0129269bdc0a458374609953a4de4f
Julio <>
2017-10-08 13:10:42 -0600

    Input Email Form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common'
+import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
+  state = { email: '', password:'' };
+
   render(){
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <Input
+          label="Email"
+          placeholder="user@gmail.com"
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+        <CardSection>
+          
+        </CardSection>
         <CardSection>
           <Button>
             Log in
