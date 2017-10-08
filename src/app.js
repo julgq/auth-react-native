01e3b073d45f7c404247b34f49af41fa96300089
Julio <>
2017-10-08 11:15:00 -0600

    Config folders components

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {View, Text } from 'react-native';
-import { Header } from './components/common/';
+import { Header, Button } from './components/common';
 
 class App extends Component {
   render(){
     return (
       <View>
-      //<Header headerText='Authentication' />
+      <Header headerText='Authentication' />
         <Text>An App!</Text>
       </View>
     );
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,11 @@
-// Import Libraries for making a component
-
+// Import libraries for making a component
 import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a component
 const Header = (props) => {
   const { textStyle, viewStyle } = styles;
+
   return (
     <View style={viewStyle}>
       <Text style={textStyle}>{props.headerText}</Text>
@@ -20,16 +20,16 @@ const styles = {
     alignItems: 'center',
     height: 60,
     paddingTop: 15,
+    shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
-    position: 'relative',
+    position: 'relative'
   },
   textStyle: {
-    fontSize: 20,
+    fontSize: 20
   }
 };
 
-
-// Make the component available to other parts of the App
+// Make the component available to other parts of the app
 export { Header };
