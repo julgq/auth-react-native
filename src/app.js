e3e897a2670018dd9330b80cba4b7b56e4b1220e
Julio <>
2017-10-08 11:37:04 -0600

    Install firebase & Create LoginForm

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import {View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header, Button } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBb5aAZR7U31aPt-8-IUlCZsjx0CyL10Dk',
+      authDomain: 'auth-98942.firebaseapp.com',
+      databaseURL: 'https://auth-98942.firebaseio.com',
+      projectId: 'auth-98942',
+      storageBucket: 'auth-98942.appspot.com',
+      messagingSenderId: '892416382931'
+    });
+
+  }
+
   render(){
     return (
       <View>
