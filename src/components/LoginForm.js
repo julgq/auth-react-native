92f0a6439088d5fc288cfa364c127459e4d5b002
Julio <>
2017-10-10 19:16:53 -0600

    Login Validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native'
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common'
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '' };
+  state = { email: '', password: '', error: '', loading: false };
+
+  // Button Login Action
   onButtonPress(){
     const { email, password } = this.state;
 
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     // Login with email and password on firebase
     firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(this.onLoginSuccess.bind(this))
     .catch(()=> {
       // If login is fail, then try yo create and new account
       firebase.auth().createUserWithEmailAndPassword(email, password)
-        .catch(()=> {
-          // If create an account fail, then show an error.
-          this.setState({ error: 'Authentication Failed.' });
-        });
+        .then(this.onLoginSuccess.bind(this))
+        .catch(this.onLoginFail.bind(this));
+
     });
   }
 
+onLoginSuccess(){
+  this.setState({
+    email: '',
+    password: '',
+    loading: false,
+    error: ''
+  });
+}
+
+onLoginFail(){
+  this.setState({
+    error: 'Authentication Failed', loading: false});
+}
+
+  renderButton(){
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+      Log in
+      </Button>
+    );
+  }
   render(){
     return (
       <Card>
@@ -49,10 +76,9 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
+
       </Card>
     );
   };
